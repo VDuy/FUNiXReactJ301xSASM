6940bd35ea391120c53735780b656064e16f41b4
Duy Vu <ducduy23899@gmail.com>
2023-08-25 15:29:31 +0700

    test

--- Assignment01/src/App.js
+++ Assignment01/src/App.js
@@ -7,7 +7,16 @@ import Home from "./pages/home/Home";
 import Detail from "./pages/detail/Detail";
 import Search from "./pages/search/Search";
 
+//-----Thêm thư viện icon vào toàn bộ dự án--------
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 function App() {
+//-----Thêm thư viện icon vào toàn bộ dự án--------
+  library.add(fas)
+//-------------------------------------------------
   return (
     <BrowserRouter>
       <Routes>
